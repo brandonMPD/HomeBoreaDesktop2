6305f3b7151f93ca7aa9e2cc7d30c71b5a493650
Admin <Admin@S10-96645>
2019-07-19 12:52:57 -0600

    Creacion de las Interfaces para Categorias, Imagenes_Servicios y Subcategorias
    Creacion de los Dao para las Interfaces Anteriores

---
+++ src/main/java/Dao/CategoriasDao.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Dao;
+
+import Modelo.Categorias;
+import interfaces.CategoriasInterface;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Admin
+ */
+public class CategoriasDao implements CategoriasInterface {
+
+    @Override
+    public String guardarCategoria(Categorias cate) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String updateCategoria(Categorias cate) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String eliminarCategoria(Categorias cate) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Categorias buscarCategorias(int categoria_id) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public ArrayList<Categorias> listarCategorias() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
---
+++ src/main/java/Dao/SubcategoriaDao.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Dao;
+
+import Modelo.Subcategorias;
+import interfaces.SubcategoriasInterface;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Admin
+ */
+public class SubcategoriaDao implements SubcategoriasInterface {
+
+    @Override
+    public String insertSubcategia(Subcategorias sub) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String updateSubcategoria(Subcategorias sub) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String deleteSubcategoria(Subcategorias sub) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Subcategorias buscarSubcategoria(int sub_cate) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public ArrayList<Subcategorias> listarSubcategorias() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
