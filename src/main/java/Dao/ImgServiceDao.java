c18daa5be1c3055c54b0b287941d8130698ee691
Admin <Admin@S10-96645>
2019-07-25 11:35:20 -0600

    dao completado para Img_servicio

--- src/main/java/Dao/ImgServiceDao.java
+++ src/main/java/Dao/ImgServiceDao.java
@@ -9,6 +9,7 @@ import Modelo.ImagenesServicio;
 import interfaces.ImageServiceInterface;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 /**
@@ -16,41 +17,38 @@ import java.util.ArrayList;
  * @author Admin
  */
 public class ImgServiceDao implements ImageServiceInterface {
-    
+
     private String mensaje;
     private String mysql;
     private PreparedStatement ejecutar;
     private ResultSet seleccionar;
-    private int registrosAfectados=0;
-    
+    private int registrosAfectados = 0;
+
     ConexionBorea conex = new ConexionBorea();
 
     @Override
     public String saveImgService(ImagenesServicio img) {
         try {
-                conex.abrirConexion();
-                
-                mysql= "insert into imageners_servicios values(?,?,?)";
-                
-                ejecutar = conex.getMiConexion().prepareStatement(mysql);
-                
-                ejecutar.setInt(1, img.getImagenservicio_id());
-                ejecutar.setInt(2, img.getServicio_id());
-                ejecutar.setString(3, img.getImagen());
-                
-               registrosAfectados =  ejecutar.executeUpdate();
-               
-               if(registrosAfectados==0){
-                   mensaje="NO SE ENCONTRO BASE_DATOS ";
-               }
-               else{
-                   mensaje="IMGAGEN ALMACENADA";
-               }   
-        } 
-        catch (Exception e) {
-            mensaje="ERROR EN DAO_IMAGE_SERVICE_DAO : "+e;
-        }
-        finally{
+            conex.abrirConexion();
+
+            mysql = "insert into imageners_servicios values(?,?,?)";
+
+            ejecutar = conex.getMiConexion().prepareStatement(mysql);
+
+            ejecutar.setInt(1, img.getImagenservicio_id());
+            ejecutar.setInt(2, img.getServicio_id());
+            ejecutar.setString(3, img.getImagen());
+
+            registrosAfectados = ejecutar.executeUpdate();
+
+            if (registrosAfectados == 0) {
+                mensaje = "NO SE ENCONTRO BASE_DATOS ";
+            } else {
+                mensaje = "IMGAGEN ALMACENADA";
+            }
+        } catch (Exception e) {
+            mensaje = "ERROR EN DAO_IMAGE_SERVICE_DAO : " + e;
+        } finally {
             conex.cerrarConexion();
         }
         return mensaje;
@@ -60,28 +58,25 @@ public class ImgServiceDao implements ImageServiceInterface {
     public String updateService(ImagenesServicio img) {
         try {
             conex.abrirConexion();
-            
-            mysql="update imagenes_servicios set servicos_id=?, imagene=? where imagenservicio=?";
-            
+
+            mysql = "update imagenes_servicios set servicos_id=?, imagene=? where imagenservicio=?";
+
             ejecutar = conex.getMiConexion().prepareStatement(mysql);
-            
+
             ejecutar.setInt(1, img.getServicio_id());
             ejecutar.setString(2, img.getImagen());
             ejecutar.setInt(3, img.getImagenservicio_id());
-            
+
             registrosAfectados = ejecutar.executeUpdate();
-            
-            if(registrosAfectados==0){
-                mensaje="NO SE ENCONTRO EL REGISTRO ";
+
+            if (registrosAfectados == 0) {
+                mensaje = "NO SE ENCONTRO EL REGISTRO ";
+            } else {
+                mensaje = "REGISTRO MODIFICADO ";
             }
-            else{
-                mensaje="REGISTRO MODIFICADO ";
-            }
-        } 
-        catch (Exception e) {
-            mensaje="ERORR EN DAO_IMAGE_SERVICE : " +e;
-        }
-        finally{
+        } catch (Exception e) {
+            mensaje = "ERORR EN DAO_IMAGE_SERVICE : " + e;
+        } finally {
             conex.cerrarConexion();
         }
         return mensaje;
@@ -89,29 +84,26 @@ public class ImgServiceDao implements ImageServiceInterface {
 
     @Override
     public String deleteService(ImagenesServicio img) {
-        
+
         try {
             conex.abrirConexion();
-            
-            mysql="delete from imagenes_servicios where imagenservicio=?";
-            
+
+            mysql = "delete from imagenes_servicios where imagenservicio=?";
+
             ejecutar = conex.getMiConexion().prepareStatement(mysql);
-            
+
             ejecutar.setInt(1, img.getImagenservicio_id());
-            
+
             registrosAfectados = ejecutar.executeUpdate();
-            
-            if(registrosAfectados == 0){
-                mensaje="NO SE ENCONTRO EL REGISTRO";
+
+            if (registrosAfectados == 0) {
+                mensaje = "NO SE ENCONTRO EL REGISTRO";
+            } else {
+                mensaje = "REGISTRO ELIMINADO";
             }
-            else{
-                mensaje="REGISTRO ELIMINADO";
-            }
-        } 
-        catch (Exception e) {
-            mensaje="REGISTRO ELIMINADO "+e;
-        }
-        finally{
+        } catch (Exception e) {
+            mensaje = "REGISTRO ELIMINADO " + e;
+        } finally {
             conex.cerrarConexion();
         }
         return mensaje;
@@ -119,12 +111,72 @@ public class ImgServiceDao implements ImageServiceInterface {
 
     @Override
     public ImagenesServicio buscarImgService(int img_service) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+
+        ImagenesServicio service = new ImagenesServicio();
+
+        try {
+            conex.abrirConexion();
+            mysql = "select * from imagenes_servicios where imagenservicio="+img_service;
+            ejecutar = conex.getMiConexion().prepareStatement(mysql);
+            
+            seleccionar = ejecutar.executeQuery();
+            
+            while(seleccionar.next()){
+                
+                service.setImagenservicio_id(seleccionar.getInt("imagenservicio"));
+                service.setServicio_id(seleccionar.getInt("servicos_id"));
+                service.setImagen(seleccionar.getString("imagene"));
+                
+            }
+            ejecutar.close();
+            
+            
+        } catch (Exception e) {
+            System.out.println("ERROR EN BUSQUEDA_IMG_SERVICIO");
+            
+        } finally {
+            conex.cerrarConexion();
+        }
+
+        return service;
     }
 
     @Override
     public ArrayList<ImagenesServicio> listarImgServices() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+       
+        ImagenesServicio ser;
+        ArrayList<ImagenesServicio> lista = new ArrayList();
+        
+        try {
+            conex.abrirConexion();
+            mysql="select * from imagenes_servicios";
+            
+            ejecutar = conex.getMiConexion().prepareStatement(mysql);
+            
+            seleccionar = ejecutar.executeQuery();
+            
+            while(seleccionar.next()){
+                ser = new ImagenesServicio();
+                
+                ser.setImagenservicio_id(seleccionar.getInt("imagenservicio"));
+                ser.setServicio_id(seleccionar.getInt("servicos_id"));
+                ser.setImagen(seleccionar.getString("imagene"));
+                
+               lista.add(ser);  
+            }
+            
+            ejecutar.close();
+            
+        } 
+        catch (SQLException e) {
+            System.out.println("ERROR EN DAO_LISTA_SERVICIOS"+e);
+        }
+        finally{
+            conex.cerrarConexion();
+        }
+        
+        return lista;
+        
     }
-    
+
 }
