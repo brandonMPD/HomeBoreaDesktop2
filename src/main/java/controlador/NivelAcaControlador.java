2e5fa7d5f53bfc4dbdcf1b23596f977da81a735d
Admin <Admin@S10-96645>
2019-08-01 09:44:14 -0600

    Creacion del Controlador para Niveles

--- src/main/java/controlador/NivelAcaControlador.java
+++ src/main/java/controlador/NivelAcaControlador.java
@@ -5,10 +5,50 @@
  */
 package controlador;
 
+import Dao.NivelesAcademicosDao;
+import Modelo.NivelesAcademicos;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
 /**
  *
  * @EDWIN ESTUARDO LEZANA RAXÓN
  */
-public class NivelAcaControlador {
+public class NivelAcaControlador implements ActionListener, MouseListener{
+    
+    NivelesAcademicos nivel = new NivelesAcademicos();
+    NivelesAcademicosDao dao = new NivelesAcademicosDao();
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
 }
